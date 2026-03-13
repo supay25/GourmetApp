c6c1f33b610a2dc5ebf6fe16ada607981cda9b7e
Arturo Diaz <luisdiazuceda@outlook.com>
2026-03-12 22:47:58 -0600

    Conexion BD MONGO

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,1 +1,14 @@
-// Configuración de la conexión a la base de Datos
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
+export const connectDB = async()=>{
+    try {
+        const dbURI = process.env.MONGO_URI
+        mongoose.connect(dbURI)
+        console.log("Conexion Exitosa")
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+}
+
--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,15 @@
 import express from "express"
 import dotenv from "dotenv"
+import { connectDB } from "./config/db.js"
 dotenv.config()
 const app = express()
 
 const PORT = process.env.PORT
 
-app.listen(PORT, ()=>{
-    console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`)
-})
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`servidor levantado e n el puerto http://localhost:${PORT}`)
+        })
+    })
+ 
